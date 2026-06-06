4ee00dda84a4bcd54287fbeb66e0122a59d90f9a
PCDICTA <atidicta@gmail.com>
2026-06-06 10:19:35 -0500

    listo

--- LabFinalGrupo2/infra/variables.tf
+++ LabFinalGrupo2/infra/variables.tf
@@ -1,11 +1,11 @@
-variable "aws_region" {
-  description = "Region AWS"
-  type        = string
-  default     = "us-east-1"
-}
+  variable "aws_region" {
+    description = "Region AWS"
+    type        = string
+    default     = "us-east-1"
+  }
 
-variable "environment" {
-  description = "Ambiente de despliegue"
-  type        = string
-  default     = "dev"
-}
+  variable "environment" {
+    description = "Ambiente de despliegue"
+    type        = string
+    default     = "dev"
+  }
